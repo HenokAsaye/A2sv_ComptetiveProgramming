ed8bdc404c575134f80898306ce9134b8fb654f4
Henok Asaye <henokasaye77@gmail.com>
2025-02-18 15:00:17 +0300

    Created 'Array Manipulation' by A2SV Hub

---
+++ 18-Feb-2025/Array Manipulation 271580.py
@@ -0,0 +1,50 @@
+# Problem: Array Manipulation - https://www.hackerrank.com/challenges/crush/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    ans = [0] * (n+1)
+    for i,j,k in queries:
+        ans[i-1] +=k
+        ans[j] -=k
+    for l in range(1,len(ans)):
+        ans[l] = ans[l-1] + ans[l]
+    return max(ans)
+
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
