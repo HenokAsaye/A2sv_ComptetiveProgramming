e8c6cde9f0c5cb8905912c0a5a9e3d090c4d1db8
Henok Asaye <henokasaye77@gmail.com>
2025-03-04 10:05:08 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 04-Mar-2025/C - Escape-Proof Transfers 290611.py
@@ -0,0 +1,13 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+n,t,c = (map(int,input().split()))
+arr = list(map(int,input().split()))
+ans = 0
+left = 0 
+for right in range(n):
+    if arr[right] > t:
+        left = right + 1
+    c_l =  right - left +1
+    if c_l >=c:
+        ans +=1
+print(ans)
