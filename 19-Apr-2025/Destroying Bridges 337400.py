5c82691d9aff2febdbc2f7bfed26881c4d576c0f
Henok Asaye <henokasaye77@gmail.com>
2025-04-19 20:28:56 +0300

    Created 'Destroying Bridges' by A2SV Hub

---
+++ 19-Apr-2025/Destroying Bridges 337400.py
@@ -0,0 +1,10 @@
+# Problem: Destroying Bridges - https://codeforces.com/problemset/problem/1944/A
+
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    for s in range(1, n+1):
+        cuts_needed = s * (n - s)
+        if cuts_needed <= k:
+            print(s)
+            break
