12177a905b94425680ecfdcafa899a30abcf4c68
Henok Asaye <henokasaye77@gmail.com>
2025-05-15 18:03:16 +0300

    Created 'Single Number' by A2SV Hub

---
+++ 15-May-2025/Single Number 356287.py
@@ -0,0 +1,12 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(len(nums)):
+            ans ^=nums[i]
+        return ans
+
+            
+
+        
