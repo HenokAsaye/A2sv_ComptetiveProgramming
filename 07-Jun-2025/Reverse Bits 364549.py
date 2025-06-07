dd6b2eedb44ef3b11036943c6322bbcb1f7f99e4
Henok Asaye <henokasaye77@gmail.com>
2025-06-07 21:03:56 +0300

    Created 'Reverse Bits' by A2SV Hub

---
+++ 07-Jun-2025/Reverse Bits 364549.py
@@ -0,0 +1,10 @@
+# Problem: Reverse Bits - https://leetcode.com/problems/reverse-bits/
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        for _ in range(32):
+            result <<= 1        
+            result |= n & 1     
+            n >>= 1             
+        return result
